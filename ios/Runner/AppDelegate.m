15d8622e22be361522a5272a45d156feeace577b
Joe <joeyoung@guojiang.tv>
2025-12-26 16:56:07 +0800

    add

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,6 +6,7 @@
 #import "YoungSecureBeforeNavigator.h"
 #import "ShadowedSemanticsSlip.h"
 #import "LayoutSystemHeaven.h"
+#import "FilterComposeContext.h"
 
 @implementation AppDelegate
 
@@ -17,8 +18,30 @@
     [SiteSkinUnit besideRectangleBlithe];
     [YoungSecureBeforeNavigator unloadUntouchedTeamConfidentiality];
     [ShadowedSemanticsSlip whitenBorder];
+    [FilterComposeContext video:self.window];
     [LayoutSystemHeaven reuseFleet];
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-@end 
+- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    [FilterComposeContext minAuthorize:app force:deviceToken];
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return YES;
+}
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options {
+    return YES;
+}
+ 
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    [FilterComposeContext picture:application];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    [FilterComposeContext background:application];
+}
+
+
+@end
